ee7567cd358290ed191838a3220ab6dac11dc290
Buechler, Trevor E <tbuechle@iu.edu>
2024-05-12 16:22:51 -0400

    Creating new HTML, fixing headers

--- test.py
+++ test.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 def index():
     # Render the test.html template
     return render_template('index.html')
+
+@app.route('/media')
+def media():
+    # Render the media.html template
+    return render_template('media.html')
+
+@app.route('/contact')
+def contact():
+    # Render the contact.html template
+    return render_template('contact.html')
