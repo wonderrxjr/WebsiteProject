11d9a1c7e7b740b4941c7b0e8dc25a093592392c
Trevor Buechler <wonderrxjr@gmail.com>
2024-05-14 12:02:46 -0400

    Database connection testing

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+import mariadb
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+dbpassword = os.getenv('dbpass')
 
 app = Flask(__name__)
+config = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'wonderrxjr',
+    'password': dbpassword,
+    'database': 'website'
+}
 
 @app.route('/')
 def index():
@@ -9,8 +23,26 @@ def index():
 
 @app.route('/media')
 def media():
+    data = []
+    try:
+        conn = mariadb.connect(**config)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM media")
+        rows = cur.fetchall()
+        for row in rows:
+            d = {
+                'name': row[1],
+                'medium': row[2],
+                'platform': row[3],
+                'notes': row[4],
+            }
+            data.append(d)
+        print(data)
+        conn.close()
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
     # Render the media.html template
-    return render_template('media.html')
+    return render_template('media.html', media=data)
 
 @app.route('/contact')
 def contact():
