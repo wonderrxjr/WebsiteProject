b29ab36fce6c4cd293adc934f14a29c9c53d6dfc
Trevor Buechler <wonderrxjr@gmail.com>
2024-05-15 21:42:59 -0400

    Added icon and contact info

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import mariadb
 from dotenv import load_dotenv
 import os
@@ -15,6 +15,11 @@ config = {
     'password': dbpassword,
     'database': 'website'
 }
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def index():
